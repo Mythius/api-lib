48ca2c705d0b1336bf7942f2a0ef70b0af658db9
matthiassouthwick <southwickmatthias@gmail.com>
2024-08-07 20:03:21 -0600

    Integrated Google Sign in Api

--- api.js
+++ api.js
@@ -11,7 +11,28 @@ exports.public = function(app){
 exports.private = function(app){
 
 	app.get('/hello2',(req,res)=>{
-		res.json({message:"Hello "+req.session.username})
+		res.json({message:"Hello "+req.session.google_data.given_name+`<br><img src="${req.session.google_data.picture}">`})
 	})
 
 }
+
+/* session.google_data
+
+{
+  iss: 'https://accounts.google.com',
+  azp: '1016767921529-7km6ac8h3cud3256dqjqha6neiufn2om.apps.googleusercontent.com',
+  aud: '1016767921529-7km6ac8h3cud3256dqjqha6neiufn2om.apps.googleusercontent.com',
+  sub: '103589682456946370010',
+  email: 'southwickmatthias@gmail.com',
+  email_verified: true,
+  nbf: 1723080904,
+  name: 'Matthias Southwick',
+  picture: 'https://lh3.googleusercontent.com/a/ACg8ocLjdsGc7uC2mmthGuvrPpmV2AFT2U_EdiXxon8tX5QwbR7m8VYkeA=s96-c',
+  given_name: 'Matthias',
+  family_name: 'Southwick',
+  iat: 1723081204,
+  exp: 1723084804,
+  jti: 'ad27c4b889a0eb48b6ce4cf6690fca739892ca88'
+}
+
+*/
--- public/api.js
+++ public/api.js
@@ -14,3 +14,31 @@ async function request(url, data = {}) {
     let req = await fetch(url, data);
     return await req.json();
 }
+function googleAuth() {
+    return new Promise((res, rej) => {
+        google.accounts.id.initialize({
+            client_id: "1016767921529-7km6ac8h3cud3256dqjqha6neiufn2om.apps.googleusercontent.com",
+            callback: handleCredentialResponse,
+            scope: "https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile"
+        });
+        google.accounts.id.prompt();
+
+        function handleCredentialResponse(data) {
+            const idToken = data.credential;
+            const decodedToken = jwt_decode(idToken);
+            const email = decodedToken.email;
+            const name = decodedToken.name;
+            const credential = data.credential;
+            let loginSuccess = loginGoogle({email,name,credential});
+
+            res({ email, name, credential, loginSuccess});
+        }
+    });
+}
+async function loginGoogle(data){
+    let authorization = JSON.stringify(data);
+    let req = await fetch('/google-signin',{method:'POST',headers:{authorization}});
+    let dat = await req.json();
+    auth_token = dat.token;
+    return JSON.stringify(dat);
+}
